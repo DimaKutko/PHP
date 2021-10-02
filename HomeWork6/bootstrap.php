3022cfd6bbb57ba32defe7bc4c67d9f46c54a7f9
DimaKutko <dimakutko410@gmail.com>
2021-10-02 23:11:57 +0300

    Impl home work 6

---
+++ HomeWork6/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+include_once __DIR__ . '/core/functions.php';
+include_once __DIR__ . '/controllers/home_controller.php';
+include_once __DIR__ . '/controllers/modal_controller.php';
+include_once __DIR__ . '/core/router_list.php';
+include_once __DIR__ . '/core/modal_builder.php';
