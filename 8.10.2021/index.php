fa56aa2213a400728890100c363f5ea6dca0bd5b
DimaKutko <dimakutko410@gmail.com>
2021-10-08 18:06:13 +0300

    Minor upd

--- 8.10.2021/index.php
+++ 8.10.2021/index.php
@@ -3,25 +3,3 @@
 include_once __DIR__ . '/bootstrap.php';
 
 RouterList::init();
-
-// class A
-// {
-//     public static function who()
-//     {
-//         echo __CLASS__;
-//     }
-//     public static function test()
-//     {
-//         static::who();
-//     }
-// }
-
-// class B extends A
-// {
-//     public static function who()
-//     {
-//         echo __CLASS__;
-//     }
-// }
-
-// B::test();
