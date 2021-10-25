8e4824a7b88bd127288dc81bebc5c093a9554880
DimaKutko <dimakutko410@gmail.com>
2021-10-25 19:22:53 +0300

    Opened teacher project

--- 25.10.2021/resources/views/layout.blade.php
+++ 25.10.2021/resources/views/layout.blade.php
@@ -1,38 +1,125 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<!doctype html>
+<html class="no-js" lang="ru">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Minecraft Projects</title>
+    <meta name="description" content="">
+    <!---meta name="viewport" content="width=device-width, initial-scale=1"---->
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="icon" href="/favicon.png">
+    <link rel="apple-touch-icon" href="../icon.png">
 
-    <!-- SEO Meta Tags -->
-    <meta name="description" content="Free mobile app HTML landing page template to help you build a great online presence for your app which will convert visitors into users">
-    <meta name="author" content="Inovatik">
-
-    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
-    <meta property="og:site_name" content="" /> <!-- website name -->
-    <meta property="og:site" content="" /> <!-- website link -->
-    <meta property="og:title" content="" /> <!-- title shown in the actual shared post -->
-    <meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
-    <meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
-    <meta property="og:url" content="" /> <!-- where do you want your post to link to -->
-    <meta property="og:type" content="article" />
-
-    <!-- Website Title -->
-    <title>Leno - Free Mobile App Landing Page Template</title>
-
-    <!-- Styles -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/fontawesome-all.css" rel="stylesheet">
-    <link href="css/swiper.css" rel="stylesheet">
-    <link href="css/magnific-popup.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-
-    <!-- Favicon  -->
-    <link rel="icon" href="images/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,300i,400,500,600" rel="stylesheet">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/icons/icomoon.css">
+    <link rel="stylesheet" href="/icons/fontawesome-all.min.css">
+    <link rel="stylesheet" href="/css/plugins.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/dist/css/swiper.min.css">
 </head>
-@yield('content')
-
+<body>
+<!-- Page Content START -->
+<div class="page-content">
+    <nav id="main-nav" class="main-nav fixed">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="nav-header d-flex justify-content-between align-items-center">
+                        <a href="index.html" class="logo" title="LOGO">
+                            <img class="logo-img" src="../img/logo.png" alt="LOGO">
+                            <img class="alt-logo-img" src="../img/logo-alt.png" alt="LOGO">
+                        </a>
+                    </div>
+                    <div class="nav-wrap">
+                        <ul id="nav" class="nav-wrap__list menu">
+                            <li class="current"><a href="/" title="Главная">Главная</a></li>
+                            <li><a href="/about.html" title="Проекте">О Проекте</a></li>
+                            <li><a href="/donate.html" title="Донат"><span class="red-fox">Донат</span></a></li>
+                            <li><a href="/contacts.html" title="Контакты">Контакты</a></li>
+                            <div class="dropdown">
+                              <span>
+                                 <div class="drop-ed"></div>
+                              </span>
+                                <div class="dropdown-content">
+                                    <span class="arrow_box"></span>
+                                    <ul class="drop-vape">
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                        <li><a href="#">Банлист</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </ul>
+                        <div class="riglt-floats-xs">
+                            <a href="#" class="btn-login"><span class="ic-sx21"></span> Войти в аккаунт</a>
+                            <a href="/how-start.html" class="btn-startgames"><span class="ic-sx22"></span> Начать играть</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+        @yield('content')
+    <footer class="section site-footer bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-2">
+                    <img class="logo-img my-3" src="../img/logo-ft.png" alt="LOGO">
+                </div>
+                <div class="col-lg-6 col-md-6 text-center">
+                    <p class="footer-text">© ProjectNames 2018 - Все права только у меня.</p>
+                </div>
+                <div class="col-md-4">
+                    <ul class="ft-nav social-nav my-3">
+                        <li class="mx-1"><a href="javascript:void(0);" title="vk"><i class="fa fa-vk"></i></a></li>
+                        <li class="mx-1"><a href="javascript:void(0);" title="twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li class="mx-1"><a href="javascript:void(0);" title="facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li class="mx-1"><a href="javascript:void(0);" title="instagram"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                    <a href="http://larts.pro/">
+                        <span class="corp-design"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- Page Content END -->
+<script src="/dist/js/swiper.min.js"></script>
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        hashNavigation: {
+            watchState: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+</script>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/circle-progress.js"></script>
+<script src="/js/examples.js"></script>
+<script src="/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="/js/vendor/jquery-3.2.1.min.js"></script>
+<script src="/js/plugins.js"></script>
+<script src="/js/main.js"></script>
+</body>
 </html>
