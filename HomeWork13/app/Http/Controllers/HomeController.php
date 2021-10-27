dfbba47ae905703157780e3c6905f49da8e16d61
DimaKutko <dimakutko410@gmail.com>
2021-10-27 12:53:05 +0300

    Update home page

--- HomeWork13/app/Http/Controllers/HomeController.php
+++ HomeWork13/app/Http/Controllers/HomeController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index', []);
+
+        // $routeCollection = Route::getRoutes();
+        $routeCollection = [];
+
+        // var_dump($routeCollection);
+
+        // $routes = array();
+        // $count = 0;
+
+        // foreach ($routeCollection->actionList as $key => $value) {
+        //     if (str_contains($key, 'App\\Http\\Controllers\\')) {
+        //         $count++;
+        //     }
+        // }
+        // echo $count;
+
+        return view('home.index', ['routes' => $routeCollection]);
     }
 }
--- HomeWork13/resources/views/home/index.blade.php
+++ HomeWork13/resources/views/home/index.blade.php
@@ -4,6 +4,10 @@
 @section('content')
 
 <div class='container'>
-    Home page
+    <?php foreach ($routes as $value) { ?>
+        <div class="alert alert-success" role="alert">
+            <?= $value->getPath() ?>
+        </div>
+    <?php } ?>
 </div>
 @endsection
