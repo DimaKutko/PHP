111eac85c25adf289d2e23e22bb05ddfe1f8a28a
DimaKutko <dimakutko410@gmail.com>
2021-10-27 11:58:59 +0300

    Create Controllers

---
+++ HomeWork13/app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function show()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
