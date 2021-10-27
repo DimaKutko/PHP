a1fa1b25dcbd0d1cac6e03be8c3803252afb4122
DimaKutko <dimakutko410@gmail.com>
2021-10-27 12:53:59 +0300

    Impl table view

--- HomeWork13/resources/views/products/table.blade.php
+++ HomeWork13/resources/views/products/table.blade.php
@@ -1,1 +1,24 @@
-123
+<div id='productsTable' class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Article</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($products as $key => $product) { ?>
+                <tr>
+                    <th name='id' product='<?= $key ?>' scope="row"><?= $key ?></th>
+                    <td><?= $product->name ?></td>
+                    <td><?= $product->sku ?></td>
+                    <td>
+                        <button type="submit" class="btn btn-danger" onclick="deleteProduct(<?= $key ?>, '<?= $product->name ?>')">Remove</button>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
