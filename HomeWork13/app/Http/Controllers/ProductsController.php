baadc1255dfad98e7c0ac3b0f8e8c2f48361fa17
DimaKutko <dimakutko410@gmail.com>
2021-10-27 12:55:07 +0300

    Create template for ProductsController methods

--- HomeWork13/app/Http/Controllers/ProductsController.php
+++ HomeWork13/app/Http/Controllers/ProductsController.php
@@ -8,21 +8,30 @@ class ProductsController extends Controller
 {
     public function index()
     {
+        return view('products.index', [
+            'products' => []
+        ]);
     }
 
     public function show()
     {
+        return view('products.show');
     }
 
     public function create()
     {
+        redirect('\products');
     }
 
     public function delete()
     {
+        redirect('\products');
     }
 
     public function renderProductTable()
     {
+        return view('products.table', [
+            'products' => []
+        ]);
     }
 }
