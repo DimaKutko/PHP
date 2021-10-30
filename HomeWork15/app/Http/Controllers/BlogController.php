89c2c445e3d36893c126e049e015fbbb1200b5bc
DimaKutko <dimakutko410@gmail.com>
2021-10-30 17:13:53 +0300

    Impl show content from db

--- HomeWork15/app/Http/Controllers/BlogController.php
+++ HomeWork15/app/Http/Controllers/BlogController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index()
     {
-        return view('blog.index');
+        return view('blog.index', [
+            'posts' => Post::all(),
+        ]);
     }
     public function about()
     {
